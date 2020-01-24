269b314889ec45caa2cfa0f394cf26c6cb0fab3e
Michael R.F <mrflowers22@uwalumni.com>
2020-01-24 10:25:42 -0800

    added network folder and network manager class

---
+++ MovieNightApp/MovieNightApp/Netowking/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  MovieNightApp
+//
+//  Created by Michael Flowers on 1/24/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
