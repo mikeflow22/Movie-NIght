80ef9bc827ddf30a0638971d5dc17068aa295295
Michael R.F <mrflowers22@uwalumni.com>
2020-01-24 16:31:36 -0800

    made  a movie model

---
+++ MovieNightApp/MovieNightApp/model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  MovieNightApp
+//
+//  Created by Michael Flowers on 1/24/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var didPickMovie: Bool = false
+    var seletedMovie: Movie?
+    var identifier = UUID().uuidString
+    
+}
