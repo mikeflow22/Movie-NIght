0de1b9afd2827e32e41f856e3923969115486adc
Michael R.F <mrflowers22@uwalumni.com>
2020-01-25 18:26:09 -0800

    added a Genre class

---
+++ MovieNightApp/MovieNightApp/model/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  MovieNightApp
+//
+//  Created by Michael Flowers on 1/25/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Codable {
+    let name: String
+    let id: Int
+}
