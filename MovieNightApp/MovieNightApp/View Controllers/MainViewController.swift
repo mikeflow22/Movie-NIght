c56968a494455d6b4b0fb450c6196c29ef29605f
Michael R.F <mrflowers22@uwalumni.com>
2020-01-27 13:29:05 -0800

    added mainViewController file and  assigned it to the view scene

---
+++ MovieNightApp/MovieNightApp/View Controllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MovieNightApp
+//
+//  Created by Michael Flowers on 1/27/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    @IBAction func searchButtonTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
