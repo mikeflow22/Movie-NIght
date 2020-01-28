3b1a8c0312eb2cdf0ba3b646bcfe67ce03c06f31
Michael R.F <mrflowers22@uwalumni.com>
2020-01-27 21:16:34 -0800

    implemented a custom sort movie table view cell class and custom outlets

--- MovieNightApp/MovieNightApp/Storyboards/SortMovieViewController.swift
+++ MovieNightApp/MovieNightApp/Storyboards/SortMovieViewController.swift
@@ -33,25 +33,25 @@ class SortMovieViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ids in viewdid load: \(selectedIds)")
-    
-            network.fetchMoviesBy(genres: selectedIds) { (movies, error) in
-                if let error = error {
-                    print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
-                    return
-                }
-                if let movies = movies {
-                    self.movies = movies
-                    print("movies count: \(movies.count)")
-                } else {
-                    print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-                }
+        
+        network.fetchMoviesBy(genres: selectedIds) { (movies, error) in
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                return
             }
+            if let movies = movies {
+                self.movies = movies
+                print("movies count: \(movies.count)")
+            } else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+            }
+        }
     }
     
-   func populateTableViews(movies: [Movie]) -> [Movie]{
-    guard !movies.isEmpty else {  print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-        return []
-    }
+    func populateTableViews(movies: [Movie]) -> [Movie]{
+        guard !movies.isEmpty else {  print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+            return []
+        }
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             let movies = network.sortMoviesByRatings(movies)
@@ -67,6 +67,9 @@ class SortMovieViewController: UIViewController {
             return []
         }
     }
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
     
 }
 
@@ -76,25 +79,43 @@ extension SortMovieViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "sortMovieCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sortMovieCell", for: indexPath) as! SortMovieCell
+        
         guard let movie = movies?[indexPath.row] else { return UITableViewCell() }
-          switch segmentedControl.selectedSegmentIndex {
-              case 0:
-                print("0")
-                  cell.textLabel?.text = movie.title
-                  cell.detailTextLabel?.text = "Ratings: \(movie.voteAverage)"
-              case 1:
-                 print("1")
-                  cell.textLabel?.text = movie.title
-                  cell.detailTextLabel?.text = "Release Date: \(movie.date)"
-              case 2:
-                 print("2")
-                  cell.textLabel?.text = movie.title
-                  cell.detailTextLabel?.text = "Popularity: \(movie.popularity)"
-              default:
-                  print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-                  return UITableViewCell()
-              }
+   
+        
+        network.fetchMoviePosterFor(movie: movie) { (image, error) in
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                return
+            }
+            if let returnedImage = image {
+                
+                DispatchQueue.main.async {
+                    switch self.segmentedControl.selectedSegmentIndex {
+                    case 0:
+                        print("0")
+                        cell.textLabel?.text = movie.title
+                        cell.detailTextLabel?.text = "Ratings: \(movie.voteAverage)"
+                        cell.imageView?.image = returnedImage
+                    case 1:
+                        print("1")
+                        cell.textLabel?.text = movie.title
+                        cell.detailTextLabel?.text = "Release Date: \(movie.releaseDate)"
+                        cell.imageView?.image = returnedImage
+                    case 2:
+                        print("2")
+                        cell.textLabel?.text = movie.title
+                        cell.detailTextLabel?.text = "Popularity: \(movie.popularity)"
+                        cell.imageView?.image = returnedImage
+                    default:
+                        print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                        break
+                    }
+                }
+            }
+        }
+        
         
         return  cell
     }
---
+++ MovieNightApp/MovieNightApp/Views /SortMovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  SortMovieCell.swift
+//  MovieNightApp
+//
+//  Created by Michael Flowers on 1/27/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class SortMovieCell: UITableViewCell {
+    var movie: Movie?
+    
+    @IBOutlet weak var moiveImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var propertyLabel: UILabel!
+    
+    @IBOutlet weak var overViewTextView: UITextView!
+    
+//    func updateViews() {
+//        guard let passedInMovie = movie, let image
+//        
+//    }
+}
